eeb1fb28101d1ffc822e85ba6b0a6ec6cace52e2
ReZhu0 <117937494+ReZhu0@users.noreply.github.com>
2022-12-16 09:50:15 -0800

    Description

--- MyWorld.java
+++ MyWorld.java
@@ -2,9 +2,10 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class MyWorld here.
+ * Visual Novel
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Rebecca Zhu 
+ * @version Dec 16 2022
  */
 public class MyWorld extends World
 {
