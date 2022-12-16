013201ae750054f592db0b56d87fe78b64396f74
ReZhu0 <117937494+ReZhu0@users.noreply.github.com>
2022-12-16 09:58:37 -0800

    size

--- Title.java
+++ Title.java
@@ -16,6 +16,6 @@ public class Title extends World
     public Title()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(1000, 800, 1); 
+        super(1000, 500, 1); 
     }
 }
