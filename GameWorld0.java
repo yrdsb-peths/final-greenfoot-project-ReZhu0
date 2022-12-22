95e812da4ecccff73375c2754f97adfcbae80c9f
ReZhu0 <117937494+ReZhu0@users.noreply.github.com>
2022-12-22 18:13:37 -0500

    Text fixed

--- GameWorld0.java
+++ GameWorld0.java
@@ -9,9 +9,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class GameWorld0 extends World
 {
-    Label text = new Label("Hello", 100);
-    Label nameEve = new Label("Eve",70);
-    
+    Label text = new Label("Hello", 35);
+    Label nameEve = new Label("Eve",45);
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -24,8 +23,22 @@ public class GameWorld0 extends World
         EveNormal eveNormal = new EveNormal();
         addObject(eveNormal,500,250);
         
-        TextBox text = new TextBox();
-        addObject (text , 500, 250);
+        TextBox textbox = new TextBox();
+        addObject (textbox , 500, 250);
+        //How the text should look
+        addObject (text, 210, 390);
+        //How the name should look
+        addObject (nameEve, 210, 332);
         
+    }
+    
+    public void act ()
+    {
+        if(Greenfoot.isKeyDown("space"))
+        {
+            removeObject(text);
+            Label text1 = new Label("How are you?", 35);
+            addObject (text1, 266, 390);
+        }
     }
 }
