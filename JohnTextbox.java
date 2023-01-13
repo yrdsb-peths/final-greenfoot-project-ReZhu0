9b2b66be51531fac9ea34bfa05b40a204344a460
ReZhu0 <117937494+ReZhu0@users.noreply.github.com>
2023-01-13 18:32:22 -0500

    More Textboxes

---
+++ JohnTextbox.java
@@ -0,0 +1,31 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class JohnTextbox here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class JohnTextbox extends Actor
+{
+    /**
+     * Act - do whatever the JohnTextbox wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    Label mainLabel = new Label("", 35);
+    
+    public JohnTextbox()
+    {
+        
+    }
+    
+    public void addedToWorld(World world)
+    {
+        world.addObject(mainLabel, 500, 410);
+    }
+    
+    public void setText(String text)
+    {
+        mainLabel.setValue(text);
+    }
+}
